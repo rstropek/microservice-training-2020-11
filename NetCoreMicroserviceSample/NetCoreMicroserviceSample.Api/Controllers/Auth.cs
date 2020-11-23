4a66d6edb9f57c64110e2c1aa9a740173d5cc62c
Rainer Stropek <rainer@software-architects.at>
2020-11-23 16:32:52 +0100

    Add OpenID Connect, login, logout, profile

---
+++ NetCoreMicroserviceSample/NetCoreMicroserviceSample.Api/Controllers/Auth.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Net.Mime;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace NetCoreMicroserviceSample.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Auth : ControllerBase
+    {
+        /// <summary>
+        /// Trigger login process
+        /// </summary>
+        [HttpGet("login", Name = "Login")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task Login()
+        {
+            await HttpContext.ChallengeAsync("IdentityServer", new AuthenticationProperties() { RedirectUri = "/" });
+        }
+
+        /// <summary>
+        /// Trigger logout process
+        /// </summary>
+        [HttpGet("logout", Name = "Logout")]
+        [Authorize]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("IdentityServer");
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+        /// <summary>
+        /// Get user profile of currently signed in user
+        /// </summary>
+        /// <returns>User profile</returns>
+        [HttpGet("profile", Name = "GetProfile")]
+        [Authorize]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(UserProfile), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public IActionResult GetProfile()
+        {
+            var identity = HttpContext.User.Identities.First();
+            return Ok(new UserProfile(
+                identity.Claims.First(c => c.Type == ClaimTypes.Name).Value,
+                identity.Claims.First(c => c.Type == ClaimTypes.Email).Value,
+                identity.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value));
+        }
+    }
+}
--- NetCoreMicroserviceSample/NetCoreMicroserviceSample.Api/Startup.cs
+++ NetCoreMicroserviceSample/NetCoreMicroserviceSample.Api/Startup.cs
@@ -1,9 +1,21 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using NetCoreMicroserviceSample.Api.Controllers;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace NetCoreMicroserviceSample.Api
 {
@@ -20,6 +32,82 @@ namespace NetCoreMicroserviceSample.Api
         {
             services.AddSingleton<IBusinessLogic, BusinessLogic>();
 
+            // Cookie configuration for HTTPS
+            services.Configure<CookiePolicyOptions>(options => options.MinimumSameSitePolicy = SameSiteMode.None);
+
+            // Add HTTP client used to get tokens from identity server
+            services.AddHttpClient("identity-server", c => c.BaseAddress = new Uri(Configuration["Oidc:Domain"]));
+
+            // Add authentication services see https://identityserver4.readthedocs.io
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect("IdentityServer", options =>
+            {
+                // Set the authority to your domain
+                options.Authority = Configuration["Oidc:Domain"];
+
+                // Configure the Client ID and Client Secret
+                options.ClientId = Configuration["Oidc:ClientId"];
+                options.ClientSecret = Configuration["Oidc:ClientSecret"];
+
+                // Set response type to code
+                options.ResponseType = OpenIdConnectResponseType.Code;
+
+                // Configure the scope
+                options.Scope.Clear();
+                options.Scope.Add("openid profile email");
+
+                // Set the callback path
+                options.CallbackPath = new PathString("/callback");
+
+                // Configure the Claims Issuer to be Auth0
+                options.ClaimsIssuer = Configuration["Oidc:Domain"];
+
+                options.Events = new OpenIdConnectEvents
+                {
+                    OnAuthorizationCodeReceived = context =>
+                    {
+                        if (Debugger.IsAttached)
+                        {
+                            // Do NOT write access codes to logs. This is for training purposes only.
+                            Debug.WriteLine($"Received code {context.TokenEndpointRequest.Code}, requesting access token");
+                        }
+
+                        return Task.CompletedTask;
+                    },
+                    OnTokenValidated = async context =>
+                    {
+                        if (Debugger.IsAttached)
+                        {
+                            // Do NOT write access tokens to logs. This is for training purposes only.
+                            Debug.WriteLine($"Token {context.TokenEndpointResponse.AccessToken} validated");
+                        }
+
+                        // Get profile information and add it to claims
+                        using var client = new HttpClient();
+                        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", context.TokenEndpointResponse.AccessToken);
+                        var response = await client.GetAsync(new Uri($"{Configuration["Oidc:Domain"]}/connect/userinfo"));
+                        response.EnsureSuccessStatusCode();
+                        var profile = await response.Content.ReadFromJsonAsync<UserProfile>();
+                        context.Principal!.Identities.First().AddClaim(new Claim(ClaimTypes.Email, profile!.Email));
+                        context.Principal!.Identities.First().AddClaim(new Claim(ClaimTypes.Name, profile.Name));
+                    },
+                    OnRedirectToIdentityProviderForSignOut = (context) =>
+                    {
+                        // End session on identity server
+                        context.Response.Redirect($"{Configuration["Oidc:Domain"]}/connect/endsession");
+                        context.HandleResponse();
+
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
             services.AddControllers();
         }
 
@@ -34,6 +122,7 @@ namespace NetCoreMicroserviceSample.Api
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
